833fdafae1259fd0f6bf7a504a4276436364a9ac
Vasyl Pavelko <vasyl.pavelko@gloriumtech.com>
2024-06-07 11:36:33 +0300

    sidebar genres with imges

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,23 @@
+import { HStack, List, ListItem, Image, Text, Spinner } from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
+import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-    const { data } = useGenres();
+    const { data, isLoading, error } = useGenres();
+
+    if (error) return null;
+    if (isLoading) return <Spinner />
 
     return (
-        <ul>
-            {data.map(genre => <li key={genre.id}>{genre.name}</li>)}
-        </ul>
+        <List>
+            {data.map(genre =>
+                <ListItem key={genre.id} paddingY='5px'>
+                    <HStack>
+                        <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)} />
+                        <Text fontSize='lg'>{genre.name}</Text>
+                    </HStack>
+                </ListItem>)}
+        </List>
     )
 }
 
